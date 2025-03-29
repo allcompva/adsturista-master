a4fb6e4cdd2de72f5135fba631adac1860b9f907
Ignacio Martin Velez <sistemas@villaallende.gov.ar>
2025-03-28 21:56:15 -0300

    apikey

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,13 +1,6 @@
 import { initializeApp } from "firebase/app";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyChr4TfLmVMaP5oPQwmJa71tR0f_l4BgnY",
-  authDomain: "steam-canto-409803.firebaseapp.com",
-  projectId: "steam-canto-409803",
-  storageBucket: "steam-canto-409803.firebasestorage.app",
-  messagingSenderId: "572084472316",
-  appId: "1:572084472316:android:857dc2a1b981c2540a8a79",
-};
+
 
 const app = initializeApp(firebaseConfig);
 
